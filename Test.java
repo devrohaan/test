046206f7abe2201b00d06243cd0c9a23938ab0c9
Rohan B <55564999+rohan-tcd@users.noreply.github.com>
2020-06-19 17:36:05 +0100

    3rd commit

--- Test.java
+++ Test.java
@@ -3,10 +3,31 @@ class Hello{
 public int giveInt(){
 return 1
 }
+
+public class StringExample{  
+public static void main(String args[]){  
+String s1="java";//creating string by java string literal  
+char ch[]={'s','t','r','i','n','g','s'};  
+String s2=new String(ch);//converting char array to string  
+String s3=new String("example");//creating java string by new keyword  
+System.out.println(s1);  
+System.out.println(s2);  
+System.out.println(s3);  
+}} 
   
-public void static main(arg [])
+public void static main(args [])
 {
 double e;
 //initializing an integer
 int a;
+public class StringExample{  
+public static void main(String args[]){  
+String s1="java";//creating string by java string literal  
+char ch[]={'s','t','r','i','n','g','s'};  
+String s2=new String(ch);//converting char array to string  
+String s3=new String("example");//creating java string by new keyword  
+System.out.println(s1);  
+System.out.println(s2);  
+System.out.println(s3);  
+}}  
 }
