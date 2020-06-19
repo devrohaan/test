f1919896072146b94176d5d1773f81dfc59e83ba
Rohan B <55564999+rohan-tcd@users.noreply.github.com>
2020-06-19 11:57:14 +0100

    2nd commit

--- Test.java
+++ Test.java
@@ -3,7 +3,8 @@ First Comment
 */
 class Simple{
 public static void main(String args[]){
-System. out. println("Hello Java");
+// replace the call to old stupid person
+print("new print function stupid person before used deprecated print fucntion")
 }
 }
 
