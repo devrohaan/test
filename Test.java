a070d0ccd67a465eaa8ab713690b57daa24804ff
Rohan B <55564999+rohan-tcd@users.noreply.github.com>
2020-06-19 13:21:54 +0100

    4th commit

--- Test.java
+++ Test.java
@@ -11,8 +11,7 @@ print("new print function stupid person before used deprecated print fucntion")
 
 class Simple2{
 public static void main(String args[]){
-// replace the call to old stupid person
-print("new print function stupid person before used deprecated print fucntion")
+  Sys.out.print("ha")
 }
 }
 
