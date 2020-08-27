ef4b5b08d845c0ffaf3e2aeb0ec0288972c95941
Rohan B <55564999+rohan-tcd@users.noreply.github.com>
2020-08-27 13:35:40 +0100

    Update Test.java

--- Test.java
+++ Test.java
@@ -13,6 +13,17 @@ String s3=new String("example");//creating java string by new keyword
 System.out.println(s1);  
 System.out.println(s2);  
 System.out.println(s3);  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  Test
 }} 
   
 public void static main(args [])
@@ -24,8 +35,7 @@ String s1="java";//creating string by java string literal
 char ch[]={'s','t','r','i','n','g','s'};  
 String s2=new String(ch);//converting char array to string  
 String s3=new String("example");//creating java string by new keyword  
-System.out.println(s1);  
-System.out.println(s2);  
+  
 System.out.println(s3);  
 }}  
 }
