80b855af07fac9cbd398cc9b2bd7e613d0c2a54f
Rohan B <55564999+rohan-tcd@users.noreply.github.com>
2020-08-31 16:45:27 +0100

    Added User Input feature

--- Calculator.java
+++ Calculator.java
@@ -1,14 +1,15 @@
-
+import java.util.Scanner;
 
 public class Calculator {
 
     public static void main(String[] args) {
-
+        
+        Scanner reader = new Scanner(System.in);
         System.out.print("Enter two numbers: ");
         double first = reader.nextDouble();
         double second = reader.nextDouble();
         System.out.print("Select Operation (+, -, *): ");
-        
+        char operation = reader.next().charAt(0);
         double result;
         switch(operation)
         {
