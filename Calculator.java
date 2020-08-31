cfe1995f9fb8ded0e643a5193b0c17638387ec49
Rohan B <55564999+rohan-tcd@users.noreply.github.com>
2020-08-31 15:24:15 +0100

    updated add and subtract operations

---
+++ Calculator.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter two numbers: ");
+        double first = reader.nextDouble();
+        double second = reader.nextDouble();
+        System.out.print("Select Operation (+, -,): ");
+        char operation = reader.next().charAt(0);
+        double result;
+        switch(operation)
+        {
+            case '+':
+                System.out.println("Result: " + (first + second));
+                break;
+            case '-':
+                System.out.println("Result: " + (first - second));
+                break;
+            default:
+                System.out.println("Error! Operation is not correct");
+                return;
+        }
+    }
+}
